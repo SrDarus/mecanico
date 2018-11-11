9f04ca135eaf12201f6d5b8d313faa09439336f7
srDarus <claudio.vargas.lillo@gmail.com>
2018-11-11 15:08:31 -0300

    login incompleto

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  	selector: 'app-login',
+  	templateUrl: './login.component.html',
+  	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	loginForm: FormGroup;
+	submitted:boolean = false;
 
-  ngOnInit() {
-  }
+  	constructor(private formBuilder:FormBuilder) { }
+
+  	ngOnInit() {
+  		this.loginForm = this.formBuilder.group({
+  			nombre: ['', Validators.required],
+  			password: ['', [Validators.required, Validators.minLength(6)]]
+  		});
+  	}
+
+  	get f(){ return this.loginForm.controls}
+
+  	login(){
+  		this.submitted = true;
+
+  		if(this.loginForm.invalid){
+  			return;
+  		}
+  	}
 
 }
